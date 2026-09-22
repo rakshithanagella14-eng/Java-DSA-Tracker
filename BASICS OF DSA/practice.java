4b6477a5132bfc723ec1ecf765e8084262f62eaa
Rakshitha <rakshithanagella14@gmail.com>
2026-09-22 22:34:36 +0530

    "Added practice java file for a day"

---
+++ BASICS OF DSA/practice.java
@@ -0,0 +1,19 @@
+class A 
+{
+    public void show()
+    {
+        System.out.println("hello");
+    }
+
+}
+
+
+
+public class practice {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+    }
+        
+
+}
