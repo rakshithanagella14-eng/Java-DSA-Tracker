915a818b6b939121c6494acf12714636b7e43cfb
Rakshitha <rakshithanagella14@gmail.com>
2026-09-23 22:09:44 +0530

    "Added switchLoop to find which day of week "

---
+++ BASICS OF DSA/switchLoop.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class switchLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        switch(n)
+        {
+            case 1 :
+                System.out.println("monday");
+                break;
+            case 2 :
+                System.out.println("tueday");
+                break;
+            case 3 :
+                System.out.println("wednesday");
+                break;
+            case 4 :
+                System.out.println("thurday");
+                break;
+            case 5 :
+                System.out.println("friday");
+                break;
+            case 6 :
+                System.out.println("saturday");
+                break;
+            default :
+                System.out.println("sunday");
+                break; }
+        sc.close();
+
+    }
+}   
